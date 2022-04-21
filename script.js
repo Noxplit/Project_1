79974d0f7901112dca8d84fcaf842f899e91e37e
Noxplit <noxplit2019@gmail.com>
2022-04-21 15:33:26 +0300

    first

---
+++ script.js
@@ -0,0 +1,12 @@
+// const result = confirm('Are you here?')
+// const result2 = +prompt('Вам есть 18?', '10')
+
+// console.log(result2 + 5);
+
+const answers = [];
+
+answers[0] = prompt('Как ваше имя?' , '')
+answers[1] = prompt('Как ваша фамилия?' , '')
+answers[2] = prompt('Сколько вам лет?' , '')
+
+document.write(answers);
